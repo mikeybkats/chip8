82a43e90ef9fafe10b80b4d9890a1865e8ca7097
mikeybkats <michaelbkats@gmail.com>
2023-07-12 21:29:05 -0700

    adding font and method to get character

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use crate::components::Memory;
 
 mod components;
+mod font;
 
 fn main() {
     let memory = Memory::new();
     assert!(memory.length() == 4096);
+
+    let font = font::Font::new();
+    assert_eq!(
+        font.get_character("1").unwrap().clone(),
+        ["0x20", "0x60", "0x20", "0x20", "0x70"]
+    )
 }
