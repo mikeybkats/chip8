810f5cc4e1188dffed7da86fe5ae6b504723f628
mikeybkats <michaelbkats@gmail.com>
2023-08-05 20:54:15 -0700

    adding clock_emulator

--- src/components.rs
+++ src/components.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
 // // One 16-bit index register called “I” which is used to point at locations in memory
 // pub struct IRegister {}
 // impl IRegister {}
@@ -7,8 +10,75 @@
 // impl Stack {}
 
 // // An 8-bit delay timer which is decremented at a rate of 60 Hz (60 times per second) until it reaches 0
-// pub struct DelayTimer {}
-// impl DelayTimer {}
+// The delay timer is active whenever the delay timer register (DT) is non-zero. This timer does nothing more than subtract 1 from the value of DT at a rate of 60Hz. When DT reaches 0, it deactivates.
+pub struct DelayTimer {
+    register: bool,
+    dt: u8,
+}
+
+impl DelayTimer {
+    pub fn new() -> DelayTimer {
+        DelayTimer {
+            register: false,
+            dt: 60,
+        }
+    }
+
+    pub fn set_timer(&mut self, run: bool) {
+        self.register = run;
+        self.update();
+    }
+
+    pub fn get_time(&self) -> u8 {
+        self.dt
+    }
+
+    fn update(&mut self) {
+        if self.register == true {
+            self.run_timer()
+        }
+    }
+
+    // this will not work as implemented here.
+    // the timer must run independent of the other parts of the application
+    /**
+    *
+    *
+       let mut dt = components::DelayTimer::new();
+       dt.set_timer(true);
+
+       loop {
+           // infinite loop
+           let t = dt.get_time();
+           if t == 1 {
+               dt.set_timer(false);
+               break;
+           }
+       }
+    */
+    fn run_timer(&mut self) {
+        let delay = Duration::from_secs_f32(1.0 / 60.0);
+
+        loop {
+            let start_time = std::time::Instant::now();
+
+            // Your loop code goes here
+            self.dt -= 1;
+            println!("delay timer: {}", self.dt);
+
+            if self.dt == 0 || self.register == false {
+                break;
+            }
+
+            let elapsed_time = start_time.elapsed();
+            if elapsed_time < delay {
+                thread::sleep(delay - elapsed_time);
+            } else {
+                println!("Loop took longer than expected!");
+            }
+        }
+    }
+}
 
 // // An 8-bit sound timer which functions like the delay timer, but which also gives off a beeping sound as long as it’s not 0
 // pub struct SoundTimer {}
@@ -16,5 +86,45 @@
 
 // // 16 8-bit (one byte) general-purpose variable registers numbered 0 through F hexadecimal, ie. 0 through 15 in decimal, called V0 through VF
 // // VF is also used as a flag register; many instructions will set it to either 1 or 0 based on some rule, for example using it as a carry flag
-// pub struct GeneralRegisters {}
-// impl GeneralRegisters {}
+pub struct GeneralRegisters {
+    v0: u8,
+    v1: u8,
+    v2: u8,
+    v3: u8,
+    v4: u8,
+    v5: u8,
+    v6: u8,
+    v7: u8,
+    v8: u8,
+    v9: u8,
+    v_a: u8,
+    v_b: u8,
+    v_c: u8,
+    v_d: u8,
+    v_e: u8,
+    v_f: u8, // flag register
+    i: u16,  // generally used to store memory addresses
+}
+impl GeneralRegisters {
+    pub fn new() -> GeneralRegisters {
+        GeneralRegisters {
+            v0: 0,
+            v1: 0,
+            v2: 0,
+            v3: 0,
+            v4: 0,
+            v5: 0,
+            v6: 0,
+            v7: 0,
+            v8: 0,
+            v9: 0,
+            v_a: 0,
+            v_b: 0,
+            v_c: 0,
+            v_d: 0,
+            v_e: 0,
+            v_f: 0,
+            i: 0,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use crate::chip8::chip8;
 // use crate::chip8::chip8;
+// use crate::chip8::chip8;
+use crate::clock::clock_emulator;
 use crate::display::Display;
-use crate::memory::Memory;
+// use crate::memory::Memory;
 // use crate::program_counter::ProgramCounter;
 
 mod chip8;
+mod clock;
+mod components;
 mod display;
 mod font;
 mod memory;
@@ -14,8 +17,8 @@ pub const WIDTH: u32 = 64;
 pub const HEIGHT: u32 = 32;
 
 fn main() {
-    let memory = Memory::new();
-    assert!(memory.length() == 4096);
+    // let memory = Memory::new();
+    // assert!(memory.length() == 4096);
 
     // let mut program_counter = ProgramCounter::new();
     // assert_eq!(program_counter.get_count(), 0);
@@ -29,5 +32,18 @@ fn main() {
     // assert_eq!(program_counter.get_count(), 0);
     // println!("program counter count: {}", program_counter.get_count());
 
-    chip8(WIDTH, HEIGHT);
+    // chip8(WIDTH, HEIGHT);
+
+    fn instruction1() -> bool {
+        println!("Running instruction 1");
+        false
+    }
+    fn instruction2() -> bool {
+        println!("Running instruction 2");
+        true
+    }
+    let instructions = vec![instruction1, instruction2];
+
+    clock_emulator(instructions);
+    println!("exiting program");
 }
